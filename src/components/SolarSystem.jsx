f1bb2a855b912a2f9ed6a9577add67207088c7cf
Thayssa Bernardo <thayssabernardo@gmail.com>
2022-04-06 15:43:34 -0300

    Conclusão requisito 4

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Header from './Header';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system">{ Header }</div>
+      <div data-testid="solar-system">
+        <Title keys="title" headline="Planetas" />
+      </div>
     );
   }
 }
