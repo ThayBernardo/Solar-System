ad9056628ca12fa72732a6a8f4c18b8b38759b92
Thayssa Bernardo <thayssabernardo@gmail.com>
2022-04-06 16:55:12 -0300

    Conclusão requisito 8 e removendo Title do arquivo App.js

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Title from './Title';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions">Teste</div>
+      <div data-testid="missions">
+        <Title keys="title" headline="Missões" />
+      </div>
     );
   }
 }
