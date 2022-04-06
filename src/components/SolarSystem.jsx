67afbba2bf2a299d5c19ff3fc3715937b2783bbd
Thayssa Bernardo <thayssabernardo@gmail.com>
2022-04-06 15:09:37 -0300

    Conclusão requisito 2

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">{ Header }</div>
+    );
+  }
+}
+
+export default SolarSystem;
